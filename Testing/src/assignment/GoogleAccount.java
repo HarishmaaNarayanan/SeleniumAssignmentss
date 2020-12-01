897ecee014e4d286f5c6696b4f7e6079d89a96fc
ELCOT <ELCOT@Lenovo>
2020-12-01 13:44:11 +0530

    Updated

---
+++ Testing/src/assignment/GoogleAccount.java
@@ -0,0 +1,28 @@
+package assignment;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleAccount {
+
+
+	WebDriver driver;
+	String url="https://www.google.com/";
+
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
+}
